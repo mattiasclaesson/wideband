3ec858f7e0328c95a8a3a05e033cbc3cdf7bdb01
mattiasclaesson <mattiaserikanna@gmail.com>
2015-07-29 07:48:02 +0200

    Moved the testapplication to a own project.

---
+++ WidebandReader/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using WidebandSupport;
+
+namespace WidebandReader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            WidebandFactory wbFactory = null;
+            IWidebandReader reader = null;
+            if (args.Length == 3)
+            {
+                wbFactory = new WidebandFactory(args[0], args[1], Boolean.Parse(args[2]));
+            }
+            else
+            {
+                wbFactory = new WidebandFactory("LM2", "COM3", false);
+            }
+
+            reader = wbFactory.CreateInstance();
+
+            reader.Start();
+
+            while (true)
+            {
+                Console.WriteLine("{0:o},{1:F2}", DateTime.Now, reader.LatestReading);
+                Thread.Sleep(TimeSpan.FromMilliseconds(100));
+            }
+        }
+    }
+}
